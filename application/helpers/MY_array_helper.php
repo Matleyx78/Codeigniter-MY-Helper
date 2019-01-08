4d40bc0e78e6024730d4c90002986ddcba833202
Matleyx78 <matmat78@hotmail.it>
2019-01-08 11:43:54 +0100

    Update MY_array_helper.php

--- application/helpers/MY_array_helper.php
+++ application/helpers/MY_array_helper.php
@@ -1,5 +1,17 @@
 <?php
-function array_msort($array, $cols)// $array = array da ordinare,$cols = colonna secondo cui ordinare Es. array_msort($data['dipendenti'], array('ass1'=>SORT_DESC));
+function array_msort($array, $cols)
+// $array = array da ordinare,$cols = colonna secondo cui ordinare Es. array_msort($data['dipendenti'], array('ass1'=>SORT_DESC));
+//ORDINAMENTO DI UN ARRAY PER COLONNA vedi:https://blog.mrwebmaster.it/2011/03/11/ordinare-un-array-multi-dimensionale-in-php.html
+    
+                //      esempio
+
+                //$utenti = array( 
+                //    array('id'=>1, 'nome'=>'Mario', 'cognome'=>'Rossi'), 
+                //    array('id'=>2, 'nome'=>'Paolo', 'cognome'=>'Bianchi'), 
+                //    array('id'=>3, 'nome'=>'Luca', 'cognome'=>'Neri'), 
+                //    array('id'=>4, 'nome'=>'Clauidia', 'cognome'=>'Bianchi'), 
+                //); 
+                //$utenti = array_msort($utenti, array('cognome'=>SORT_ASC, 'nome'=>SORT_ASC)); 
 {
     $colarr = array();
     foreach ($cols as $col => $order) {
@@ -37,39 +49,4 @@ function unique_multidim_array($array, $key)//unique_multidim_array
         $i++;
     }
     return $temp_array;
-}        
-
-function array_msort2($array, $cols) { //ORDINAMENTO DI UN ARRAY PER COLONNA vedi:https://blog.mrwebmaster.it/2011/03/11/ordinare-un-array-multi-dimensionale-in-php.html
-    $colarr = array(); 
-    foreach ($cols as $col => $order) { 
-        $colarr[$col] = array(); 
-        foreach ($array as $k => $row) { $colarr[$col]['_'.$k] = strtolower($row[$col]); } 
-    } 
-    $eval = 'array_multisort('; 
-    foreach ($cols as $col => $order) { 
-        $eval .= '$colarr[\''.$col.'\'],'.$order.','; 
-    } 
-    $eval = substr($eval,0,-1).');'; 
-    eval($eval); 
-    $ret = array(); 
-    foreach ($colarr as $col => $arr) { 
-        foreach ($arr as $k => $v) { 
-            $k = substr($k,1); 
-            if (!isset($ret[$k])) $ret[$k] = $array[$k]; 
-            $ret[$k][$col] = $array[$k][$col]; 
-        } 
-    } 
-    return $ret; 
-    
-                //      esempio
-
-                //$utenti = array( 
-                //    array('id'=>1, 'nome'=>'Mario', 'cognome'=>'Rossi'), 
-                //    array('id'=>2, 'nome'=>'Paolo', 'cognome'=>'Bianchi'), 
-                //    array('id'=>3, 'nome'=>'Luca', 'cognome'=>'Neri'), 
-                //    array('id'=>4, 'nome'=>'Clauidia', 'cognome'=>'Bianchi'), 
-                //); 
-                //$utenti = array_msort($utenti, array('cognome'=>SORT_ASC, 'nome'=>SORT_ASC)); 
-  
-    
 } 
