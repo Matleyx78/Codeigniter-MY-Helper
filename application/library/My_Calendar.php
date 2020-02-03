b1cf297ea45fd10f3e4fb559a9766f57040a4039
Matleyx78 <matmat78@hotmail.it>
2020-02-03 12:30:12 +0100

    Update My_Calendar.php

--- application/library/My_Calendar.php
+++ application/library/My_Calendar.php
@@ -1,1 +1,10 @@
-ddd
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Calendar extends CI_Calendar {
+
+  public function __construct()
+	  {
+      parent::__construct();
+	  }
+
+}
